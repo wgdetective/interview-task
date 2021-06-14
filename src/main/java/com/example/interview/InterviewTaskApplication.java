4233a6d8a1852fbafcc19153ab018c5b3e8879cf
Dzmitry Marudau <Dzmitry_Marudau@epam.com>
2021-06-15 01:16:51 +0300

    Correct package naming

--- src/main/java/com/example/interview/InterviewTaskApplication.java
+++ src/main/java/com/example/interview/InterviewTaskApplication.java
@@ -1,4 +1,4 @@
-package com.example.interview_task;
+package com.example.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
